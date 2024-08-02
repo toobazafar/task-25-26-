057e9a0b0a7dc49e9a38b823d0069a5675a4c164
TOOBA ZAFAR <160070906+toobazafar@users.noreply.github.com>
2024-08-02 22:21:27 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,15 @@
+let alien_color = "green";
+if(alien_color == "green"){
+    console.log("the player just earned 5 points.");
+    }
+    else {
+        console.log("the player not earned 5 points");
+    }
+    
+    //Write one version of this program that passes the if test and another that fails.
+    //(The version that fails will have no output.)
+    
+    //let alien_color1 = "red";
+    // if(alien_color1 == "green"){
+    // console.log("the player just earned 5 points.");
+    // }
